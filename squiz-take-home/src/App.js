567d4cdb43978e6458575fb53d884875d9de4fe8
Yash <yash592@gmail.com>
2022-06-16 23:58:13 -0700

    search fields

--- squiz-take-home/src/App.js
+++ squiz-take-home/src/App.js
@@ -1,46 +1,78 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
+const SQUIZ_URL = 'https://dujour.squiz.cloud/developer-challenge/data'
 
 function App() {
 
-  const [customer, setCustomer] = useState([])
+  const [data, setData] = useState([])
   const [country, setCountry] = useState('')
+  const [searchField, setSearchField] = useState('');
   // const [industry, setIndustry] = useState['']
-  console.log(customer)
+
+  console.log('data', data)
+
 
   useEffect(() => {
-    fetch('https://dujour.squiz.cloud/developer-challenge/data')
+    fetch(SQUIZ_URL)
       .then(response => response.json())
-      .then(data => setCustomer(data))
+      .then(data => setData(data))
       .catch(err => console.log(err))
   }, [])
 
+
+  const handleChange = (event) => {
+    const searchString = event.target.value.toLocaleLowerCase();
+    console.log(searchString)
+    setSearchField(searchString);
+
+  }
+
+  const filteredData = data.filter((clients) => {
+    return clients.name.toLocaleLowerCase().includes(searchField);
+    setData(filteredData);
+    console.log(data)
+  })
 
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <input
+          className='search-box'
+          type='search'
+          placeholder='Search By Country'
+          onChange={handleChange}
+        />
+        <input
+          className='search-box-industry'
+          type='search'
+          placeholder='Search By Industry'
+          onChange={handleChange}
+        />
         <ul>
-          {customer.map(name =>
+          {filteredData.map(name =>
             <li>{name.name}</li>
           )}
         </ul>
       </header>
+
     </div>
   );
 }
 
 export default App;
+
+
+// • Retrieve data from https://dujour.squiz.cloud/developer-challenge/data 
+// 	• Display the retrieved data as a list
+// 	• Enable filtering by "country" and "industry" values; Changing filters should not reset sort order selection
+// 	• Enable ascending and descending sorting by "name" or "numberOfEmployees" values; Changing the sort order
+//    should not reset filters selection
+// 	• On desktop, use a two column layout, showing filters and sort options on the left hand side and the list of 
+//   items on the right
+// 	• On mobile, use a single column, stacked layout, with filters and sort options above the list of items
+// 	• CSS and JavaScript should be bundled and minified. You should use a CSS preprocessor. 
+//   You can choose between building the application in regular HTML or React with JSX. You may use libraries, 
+//   but should avoid using jQuery
